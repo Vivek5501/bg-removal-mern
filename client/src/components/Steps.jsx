107bbebd68f511b7f1e193e0b0ac2e0de6b59401
Vivek5501 <2003viv10@gmail.com>
2024-10-25 21:45:35 +0530

    update

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -13,7 +13,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.upload_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Upload image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Select and upload an image from your device  <br />to start the background removal process.</p>
                     </div>
                 </div>
 
@@ -21,7 +21,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Remove background</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>With just one click, our AI will automatically  <br /> remove the background from your image.</p>
                     </div>
                 </div>
 
@@ -29,7 +29,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.download_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Download image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Download the edited image with a transparent  <br /> background or choose a new background.</p>
                     </div>
                 </div>
 
