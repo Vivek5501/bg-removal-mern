1bb99050ad147f7a45821185b906de4f4588d318
Vivek5501 <2003viv10@gmail.com>
2024-11-05 16:59:09 +0530

    update home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <Header />
             <Steps />
             <BgSlider />
-            <Testimonials />
+            {/* <Testimonials /> */}
             <Upload />
         </>
     )
