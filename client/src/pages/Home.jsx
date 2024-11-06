660f54e942ce041d30f01780943f51082b2c78e4
Vivek5501 <2003viv10@gmail.com>
2024-11-06 08:59:00 +0530

    remove steps

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <>
             <Header />
-            <Steps />
+            {/* <Steps /> */}
             <BgSlider />
             {/* <Testimonials /> */}
             <Upload />
